5fdf3f7e32c9c66d42054e15c71a42ead0827acd
SHINOATV <ia@shinoa.tv>
2016-12-14 20:33:36 +0100

    Premier IMPORT

---
+++ menu.php
@@ -0,0 +1,33 @@
+<nav id="global_nav">
+    <div id="imageCircus">
+        <img src="Images/circus_cirque.png" alt="Clown" width="350px" />
+    </div>
+
+    <div id="menu">
+
+        <div class="boite contact">
+            <a href="contact.html">CONTACT US</a>
+        </div>
+
+        <div class="boite performance3">
+            <a href="performance3.html">PERFORMANCE N3</a>
+        </div>
+
+        <div class="boite performance2">
+            <a href="performance2.html">PERFORMANCE N2</a>
+        </div>
+
+        <div class="boite performance1">
+            <a href="performance1.html">PERFORMANCE N1</a>
+        </div>
+
+        <div class="boite tarifs">
+            <a href="tarifs.html">PRICES</a>
+        </div>
+
+        <div class="boite accueil">
+            <a href="page.html">HOME</a>
+        </div>
+
+    </div>
+</nav>
